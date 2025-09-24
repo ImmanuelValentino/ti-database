fb6a534d4a6c6c833d254842a1a4cd438f711586
Immanuel V <imanuelv203@gmail.com>
2025-09-24 19:00:41 +0700

    Swagger Fix Second

--- src/app/docs/page.js
+++ src/app/docs/page.js
@@ -1,27 +1,8 @@
 "use client";
 
-import { useEffect } from "react";
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
 
-export default function SwaggerPage() {
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js";
-        script.onload = () => {
-            window.SwaggerUIBundle({
-                url: "/swagger.yaml",
-                dom_id: "#swagger-ui",
-            });
-        };
-        document.body.appendChild(script);
-    }, []);
-
-    return (
-        <div>
-            <link
-                rel="stylesheet"
-                href="https://unpkg.com/swagger-ui-dist/swagger-ui.css"
-            />
-            <div id="swagger-ui" />
-        </div>
-    );
+export default function DocsPage() {
+    return <SwaggerUI url="/swagger.yaml" />;
 }
